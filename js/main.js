08b9a19936de403413c2b98da8eea695c5c01902
Dmitry K <expatriate@inbox.ru>
2019-01-20 12:18:29 +0500

    Fixes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,71 @@
 'use strict'
 
 $(document).ready(function() {
+  var widget = $('#widget-v1');
+  var content = 
+    '<div class="menu">' +
+        '<div class="menu__item">' +
+            '<a class="link-button link-button--filled-blue popup-with-form" target="_self" href="#test-form">' +
+                '<span>' +
+                    'Предварительная регистрация' +
+                '</span>' +
+            '</a>' +
+        '</div>' +
+        '<div class="menu__item">' +
+            '<a class="link-button link-button--filled-blue popup-with-form" target="_self" href="#test-form">' +
+                '<span>' +
+                    'Расписание' +
+                '</span>' +
+            '</a>' +
+        '</div>' +
+        '<div class="menu__item">' +
+            '<a class="link-button link-button--filled-blue popup-with-form" target="_self" href="#test-form">' +
+                '<span>' +
+                    'Трансляция' +
+                '</span>' +
+            '</a>' +
+        '</div>' +
+        '<div class="menu__item">' +
+            '<a class="link-button link-button--filled-blue popup-with-form" target="_self" href="#test-form">' +
+                '<span>' +
+                    'Купить билет' +
+                '</span>' +
+            '</a>' +
+        '</div>' +
+        '<div class="menu__item">' +
+            '<a href="#" id="timepad_twf_register_888157" style="height: 40px !important; display: inline-block !important; text-decoration: none !important;"><img style="height: 40px !important;" src="https://timepad.ru/img/twf2/btns/twfbtn1.png"/></a><script async="async" defer="defer" charset="UTF-8" data-timepad-customized="44466" data-timepad-widget-v2="event_register" src="https://timepad.ru/js/tpwf/loader/min/loader.js">(function(){return {"event" : {"id" : 888157 }, "hidePreloading" : true, "display" : "popup", "popup" : {"triggerSelector" : "#timepad_twf_register_888157"}}})();</script>' +
+        '</div>' +
+    '</div>';
+
+    widget.append(content);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   $('.popup-with-form').magnificPopup({
     type: 'inline',
     preloader: false,
@@ -11,7 +76,7 @@ $(document).ready(function() {
     callbacks: {
       beforeOpen: function() {
         console.log(window.parent)
-        window.parent.resetFrameSize($(window).height(),$(window).width())
+        //window.parent.resetFrameSize($(window).height(),$(window).width())
         if($(window).width() < 700) {
           this.st.focus = false;
         } else {
@@ -21,3 +86,5 @@ $(document).ready(function() {
     }
   });
 });
+
+
